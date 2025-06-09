f81a1b5181f41f57f41a84f2902014982ee58c7f
pandakewt <chuminhtuan30306@gmail.com>
2025-06-09 13:41:36 +0700

    fix: fix config file and remove guides

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,19 +5,12 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://f1r3ch0.github.io/',
-	base: '',
+	base: 'f1r3ch0',
 	integrations: [
 		starlight({
-			title: 'F1r3ch0	',
+			title: 'F1r3ch0',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
-				{
-					label: 'example',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
 				{
 					label: '2025',
 					items: [
