2fa92e1d913d57baaf21b78ad1879c0d1a60615e
pandakewt <chuminhtuan30306@gmail.com>
2025-06-09 13:32:02 +0700

    Change config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,21 +4,26 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://f1r3ch0.github.io/',
+	base: '',
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: '',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'example',
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Example Guide', slug: 'guides/example' },
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: '2025',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'TJCTF 2025', autogenerate: { directory: 'year-2025/tjctf'} },
+					],
 				},
 			],
 		}),
---
+++ src/content/docs/year-2025/tjctf/web-double-nested.md
@@ -0,0 +1,3 @@
+---
+title: Web - Double nested
+---
